ffa5655470a2fa597d9971e9d5c5a0940043a399
MrTrueChina <mr.true.china@gmail.com>
2019-07-10 12:04:57 +0800

    start

--- src/main/java/org/mtc/crawler/Starter.java
+++ src/main/java/org/mtc/crawler/Starter.java
@@ -2,6 +2,7 @@ package org.mtc.crawler;
 
 import java.io.IOException;
 
+import org.mtc.crawler.data.BiQuGeData;
 import org.mtc.crawler.data.ShuShuWuData;
 
 /**
@@ -13,17 +14,17 @@ import org.mtc.crawler.data.ShuShuWuData;
 public class Starter {
 	public static void main(String[] args) {
 
-		CrawlerData data = new ShuShuWuData();
+		CrawlerData data = new BiQuGeData();
 
 		data.agreement = "http://";
 		data.mainPageUrl = "http://www.shushuwuxs.org/1_1035/";
 		data.savePath = System.getProperty("user.dir") + "\\save";
 		data.userAgent = "I,crawler"; // 这是个错误示范，但这个网站真的就是没有任何检测，有需要请自行百度如何查浏览器的用户代理
 
-		data.bookNameQuery = "div#maininfo>div#info>h1"; // id为"maininfo"的div的直接子元素中的id为"info"的div的直接子类中的h1
-		data.chapterQuery = "div#list>dl>dd+dt~dd>a"; // id为"list"的div的直接子元素中的dl的直接子元素中的dd后面的dt后面的所有同父级的dd的直接子元素中的a标签
-		data.chapterNameQuery = "div.bookname>h1"; // 类名为"bookname"的div的直接子元素中的h1
-		data.textQuery = "div#content"; // id为"content"的div
+//		data.bookNameQuery = "div#maininfo>div#info>h1"; // id为"maininfo"的div的直接子元素中的id为"info"的div的直接子类中的h1
+//		data.chapterQuery = "div#list>dl>dd+dt~dd>a"; // id为"list"的div的直接子元素中的dl的直接子元素中的dd后面的dt后面的所有同父级的dd的直接子元素中的a标签
+//		data.chapterNameQuery = "div.bookname>h1"; // 类名为"bookname"的div的直接子元素中的h1
+//		data.textQuery = "div#content"; // id为"content"的div
 
 		data.minInterval = 0;
 		data.maxInterval = 1000;
--- src/main/java/org/mtc/crawler/data/BiQuGeData.java
+++ src/main/java/org/mtc/crawler/data/BiQuGeData.java
@@ -2,17 +2,14 @@ package org.mtc.crawler.data;
 
 import org.mtc.crawler.CrawlerData;
 
-/**
- * 书书屋 （http://www.shushuwuxs.org/） 的基本配置
- */
-public class ShuShuWuData  extends CrawlerData{
+public class BiQuGeData extends CrawlerData{
 
-	public ShuShuWuData() {
+	public BiQuGeData() {
 		agreement = "http://";
 		userAgent = "I,crawler"; // 这是个错误示范，但这个网站真的就是没有任何检测，有需要请自行百度如何查浏览器的用户代理
 
 		bookNameQuery = "div#maininfo>div#info>h1"; // id为"maininfo"的div的直接子元素中的id为"info"的div的直接子类中的h1
-		chapterQuery = "div#list>dl>dd+dt~dd>a"; // id为"list"的div的直接子元素中的dl的直接子元素中的dd后面的dt后面的所有同父级的dd的直接子元素中的a标签
+		chapterQuery = "div#list>dl>dd>a"; // id为"list"的div的直接子元素中的dl的直接子元素中的dd后面的dt后面的所有同父级的dd的直接子元素中的a标签
 		chapterNameQuery = "div.bookname>h1"; // 类名为"bookname"的div的直接子元素中的h1
 		textQuery = "div#content"; // id为"content"的div
 	}
